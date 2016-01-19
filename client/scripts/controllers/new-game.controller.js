bc731839856d6836f8c8c11407267f828a730ba5
Wouter Roosendaal <Wouter.Roosendaal@planonsoftware.com>
2016-01-19 16:18:16 +0100

    new game team selection

--- client/scripts/controllers/new-game.controller.js
+++ client/scripts/controllers/new-game.controller.js
@@ -7,26 +7,62 @@ function NewGameCtrl($scope, $reactive, $state, $ionicPopup, NewGame, NewTeam) {
   $reactive(this).attach($scope);
  
   this.hideNewGameModal = hideNewGameModal;
-  this.newGame = newGame;
   this.showNewTeamModal = showNewTeamModal;
+  this.newGame = newGame;
+  this.selectTeam = selectTeam;
 
   function showNewTeamModal() {
     NewTeam.showModal();
   }
   
+  this.enabled = true;
+  // this.subscribe('redteams', () => {
+  //   return [ Teams.find({ _id: { $ne: this.getReactively('this.teamBlue._id') } }) ];
+  // });
+  
+  // this.subscribe('blueteams', () => {
+  //   return [ Teams.find({ _id: { $ne: this.getReactively('this.teamRed._id') } }) ];
+  // });
+  
+  // this.helpers({
+  //   redteams: () => {
+  //     return Teams.find({ _id: { $ne: this.getReactively('this.teamBlue._id') } });
+  //   },
+  //   blueteams: () => {
+  //     return Teams.find({ _id: { $ne: this.getReactively('this.teamRed._id') } });
+  //   }
+  // });
+
   this.helpers({
-    teams() {
+    redteams: () => {
+      return Teams.find({});
+    },
+    blueteams: () => {
       return Teams.find({});
     }
   });
- 
+
   function hideNewGameModal() {
     NewGame.hideModal();
   }
   
+  function selectTeam() {
+    console.log('team red: ' + this.teamRed);
+    console.log('team blue: ' + this.teamBlue);
+    if (this.teamRed && this.teamBlue && this.teamRed._id != this.teamBlue._id) {
+      delete this.selectMessage; 
+      this.enabled = false;
+    } else {
+      if (this.teamRed && this.teamBlue) {
+        this.selectMessage = 'You can\'t select the same team twice';
+      } 
+      this.enabled = true;
+    }
+  }
+  
   function newGame() {
     try {
-        Meteor.call('newGame', {name: this.name, claim: this.claim}, function(error){
+        Meteor.call('newGame', {teamRed: this.teamRed, teamBlue: this.teamBlue}, function(error){
             if (error) {
                 var alertPopup = $ionicPopup.alert({
                     title: 'Error adding Game',
