5c2d1db64c3552187c4870126b83d2df1720dd10
Wouter Roosendaal <tuvokki@gmail.com>
2016-05-03 16:18:16 +0200

    Update new game/games list closes #17

--- imports/components/games/games.js
+++ imports/components/games/games.js
@@ -43,12 +43,41 @@ class GamesCtrl {
     this.gameScoreService.scored(team, this.inprogress);
   }
 
+  trashGameInProgressModal() {
+    if (this.inprogress) {
+      let confirmPopup = this.$ionicPopup.confirm({
+        title: 'Trash this game?',
+        template: 'This will end this game. The scored goals will not result in any change in ELO standings.<br><b>Do you want to end this game?</b>'
+      });
+
+      confirmPopup.then((res) => {
+        if (res) {
+          Games.remove(this.inprogress._id);
+        } else {
+          return;
+        }
+      });
+    } else {
+      this.$state.go('tab.newgame');
+    }
+  }
+
   showNewGameModal() {
     this.$state.go('tab.newgame');
   }
 
   remove(game) {
-    Games.remove(game._id);
+      let confirmPopup = this.$ionicPopup.confirm({
+        title: 'Delete this game?',
+        template: 'You <i>can</i> delete this game but it will have no effect on the ELO standings in the leaderboard.<br>The only reason to play-and-delete a game is for cheating, which is not really fair.<br><b>Do you want to delete this game?</b>'
+      });
+      confirmPopup.then((res) => {
+        if (res) {
+          Games.remove(game._id);
+        } else {
+          return;
+        }
+      });
   }
 
 }
