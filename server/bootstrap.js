0bc796a6354ff4a3e9abcf0f3545d81f6c25f2ca
Wouter <tuvokki@gmail.com>
2016-05-15 08:18:52 +0200

    Auto create the admin user

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,1 +1,25 @@
 //bootstrap code
+let admin = Accounts.users.findOne({
+  username: 'admin'
+});
+
+if (!admin) {
+  console.log('Creating admin user from defaults.');
+  try {
+    Accounts.createUser({
+      username: 'admin',
+      email: 'admin@tuvok.nl',
+      password: 'secret',
+      role: 'admin',
+      profile: {
+        //publicly visible fields like firstname goes here
+        firstname: 'Lt. Commander',
+        lastname: 'Tuvok'
+      }
+    });
+
+  } catch (e) {
+    console.log('Hjalp!!');
+    console.log(e);
+  }
+}
