9f5f7fd5ab24e7330208af19cbddb2932791d7f2
Wouter <tuvokki@gmail.com>
2016-05-16 09:01:32 +0200

    implement toggle admin and remove users

--- imports/components/settings/settings.js
+++ imports/components/settings/settings.js
@@ -5,11 +5,12 @@ import { Players } from '../../api/players.js';
 import template from './settings.html';
 
 class SettingsCtrl {
-  constructor($scope, $ionicPopup, $state, $log, usersService) {
+  constructor($scope, $ionicPopup, $state, $log, $ionicListDelegate, usersService) {
     $scope.viewModel(this);
     this.$ionicPopup = $ionicPopup;
     this.$state = $state;
     this.$log = $log;
+    this.$ionicListDelegate = $ionicListDelegate;
     this.usersService = usersService;
 
     console.log('in settings controller');
@@ -34,7 +35,72 @@ class SettingsCtrl {
         return Meteor.users.find()
       }
     });
+  }
 
+  remove(user) {
+    let confirmPopup = this.$ionicPopup.confirm({
+      title: 'Remove ' + user.username,
+      template: 'Remove <b>' + user.username + '</b>?'
+    });
+
+    confirmPopup.then((res) => {
+      if (res) {
+        Meteor.users.remove({ _id: user._id });
+      } else {
+        return;
+      }
+      this.$ionicListDelegate.closeOptionButtons();
+    });
+  }
+
+  toggleAdmin(user) {
+    if (user.profile.role == 'admin') {
+      this.revokeAdmin(user);
+    } else {
+      this.makeAdmin(user);
+    }
+  }
+
+  makeAdmin(user) {
+    let confirmPopup = this.$ionicPopup.confirm({
+      title: 'Make ' + user.username + ' admin?',
+      template: 'Do you really want to invest all that power to <b>' + user.username + '</b>? It will surely give some trouble.'
+    });
+
+    confirmPopup.then((res) => {
+      if (res) {
+        let adminProfile = user.profile;
+        adminProfile.role = 'admin';
+        Meteor.users.update({ _id: user._id },
+        {
+          $set: { profile: adminProfile }
+        });
+      } else {
+        return;
+      }
+      this.$ionicListDelegate.closeOptionButtons();
+    });
+  }
+
+  revokeAdmin(user) {
+    let confirmPopup = this.$ionicPopup.confirm({
+      title: 'Revoke admin powers from ' + user.username + '?',
+      template: 'Do you really want to kick <b>' + user.username + '</b> back to the realms of ordinary users? A wise descision.'
+    });
+
+    confirmPopup.then((res) => {
+      if (res) {
+        let userProfile = user.profile;
+        delete userProfile.role;
+        Meteor.users.update({ _id: user._id },
+        {
+          $set: { profile: userProfile }
+        });
+      } else {
+        return;
+      }
+      this.$ionicListDelegate.closeOptionButtons();
+    });
   }
 
   doLoginAction() {
@@ -50,7 +116,7 @@ class SettingsCtrl {
         // :(
         return that.handleError(error);
       }
-      that.$state.go('tab.players');
+      that.$state.go('tab.settings');
     });
   }
 
@@ -79,7 +145,7 @@ export default angular.module('settings', [
 ])
   .component('settings', {
     templateUrl: 'imports/components/settings/settings.html',
-    controller: ['$scope', '$ionicPopup', '$state', '$log', 'usersService', SettingsCtrl]
+    controller: ['$scope', '$ionicPopup', '$state', '$log', '$ionicListDelegate', 'usersService', SettingsCtrl]
   })
   .config(($stateProvider) => {
       $stateProvider.state('tab.settings', {
