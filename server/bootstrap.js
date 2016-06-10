35ac4211a09ced9f2205ea503eaefb0955b53738
Wouter <tuvokki@gmail.com>
2016-06-10 12:25:16 +0200

    create fixture at start

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -24,3 +24,61 @@ if (!admin) {
   }
 }
 console.log("Environment: " + process.env.NODE_ENV);
+
+import { Players } from '/imports/api/players.js';
+import { Teams } from '/imports/api/teams.js';
+import { Games } from '/imports/api/games.js';
+
+/**
+ * Randomize array element order in-place.
+ * Using Durstenfeld shuffle algorithm.
+ */
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+
+if (process.env.NODE_ENV === "development") {
+  let players = ["Antoon","Ramoen","Bortin","Tom","Gasper","Susanne",
+    "WAuter","Patterick","Maarthen","SvenA","Nicky",
+    "Ulger","SvenB","Jerom","Chat","Meloossa","Annie",
+    "Maartin","Robberd","Marc-c-c","Antonie","Barm","MarkyMark"
+    ];
+  if (Players.find().fetch().length > 0) {
+  }
+  // To start with a new database, start meteor like this
+  // CLEAN_START=true meteor
+  if (process.env.CLEAN_START) {
+    // Empty all data
+    Players.remove({});
+    Teams.remove({});
+    Games.remove({});
+
+    // Create players
+    players.forEach(function(player) {
+      Meteor.call('newPlayer', { name: player }, (error) => {
+        if (error) {
+          console.log(error);
+        }
+      });
+    }, this);
+    
+    // Create 10 random teams
+    for (var i = 0; i < 10; i++) {
+      teamplayers = shuffleArray(players);
+      p0 = Players.findOne({ name: teamplayers[0] });
+      p1 = Players.findOne({ name: teamplayers[1] });
+      Meteor.call('newTeam', { players: [p0._id, p1._id]}, (error) => {
+        if (error) {
+          console.log(error);
+        }
+      });
+    }
+    
+  }
+}
