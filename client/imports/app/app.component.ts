e7ca9a17f72c0a7c9dde1efc8a9af01d89bb8c23
Wouter Roosendaal <tuvokki@gmail.com>
2017-03-25 18:30:19 +0100

    Initial commit

---
+++ client/imports/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { StatusBar, Splashscreen } from 'ionic-native';
+
+import template from './app.component.html';
+
+@Component({
+  selector: 'my-app',
+  template
+})
+
+export class MyApp {
+  constructor(platform: Platform) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      if (platform.is('cordova')) {
+        StatusBar.styleDefault();
+        Splashscreen.hide();
+      }
+    });
+  }
+}
