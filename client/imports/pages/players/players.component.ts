bb42c06c563be9655dd7fad219f1a2f023773c63
Wouter Roosendaal <tuvokki@gmail.com>
2017-03-26 10:02:07 +0200

    Add players component

---
+++ client/imports/pages/players/players.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'players',
+  templateUrl: 'players.component.html'
+})
+export class PlayersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+}
