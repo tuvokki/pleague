8485cb4365bf0b20922384cdc4d8ab48b07d88a9
Wouter <tuvokki@gmail.com>
2018-04-20 08:24:11 +0200

    Add seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,24 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+defmodule Pleague.Data do
+  alias Pleague.Repo
+  alias Pleague.Accounts
+  alias Pleague.Accounts.User
+  alias Pleague.Accounts.Credential
+
+  IO.puts("Generating data")
+
+  user =
+    Repo.insert!(%User{
+      name: "Rainbow Trout",
+      username: "rainbow"
+      # credential: credential.id
+    })
+
+  credential =
+    Repo.insert!(%Credential{
+      email: "rainbow@trout.com",
+      user_id: user.id
+    })
+end
